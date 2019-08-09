50e7c8149d32f48cfe0776afd062d91fc72ade8c
Mark Jan van Kampen <mjvk@allseas.com>
2019-08-09 11:09:51 +0200

    Removes debug build from CI - fix

--- build.py
+++ build.py
@@ -4,6 +4,6 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(build_type=["Release"])
+    builder = ConanMultiPackager(build_types=["Release"])
     builder.add_common_builds(pure_c=True)
     builder.run()
